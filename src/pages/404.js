c106eccfca48a4e62422963eaffe2a32bd7b5cc7
Kenn Stack <kibadikennedy@gmail.com>
2021-05-16 21:42:33 +0800

    testing build command

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,10 +8,10 @@ function NotFoundPage() {
         <div className="min-h-screen">    
             <Layout>
                 <SEO title="404: Not found" description="Can't find this page"/>
-                <h1> 404: Page not Found :( </h1>
-                <Link to="/" className="flex flex-row bg-white text-mirage-500 p-3 rounded shadow mx-auto"> 
-                    <p> Go to Home Page</p> 
-                    <span> 🏡 </span>
+                <h1 className="text-2xl md:text-4xl text-white my-5 mx-auto"> Page not Found :( </h1>
+                <Link to="/" className="flex flex-row bg-white font-semibold text-mirage-500 w-60 p-3 rounded shadow mx-auto"> 
+                    <p className="text-xl"> Go to Home Page</p> 
+                    <span className="text-xl"> 🏡 </span>
                 </Link>
             </Layout>
         </div>
