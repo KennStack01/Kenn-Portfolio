5de26b17b351b6590a0de70979ecfd3381918e1d
Kenn Stack <kibadikennedy@gmail.com>
2021-05-19 17:37:43 +0800

    updated gatsby-node file for handling window object error

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,3 +28,18 @@
 //         })
 //     })
 // }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /bad-module/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
