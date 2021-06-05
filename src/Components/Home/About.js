32225979370bdc3c2f68e523ab01f644ba6153b3
Kenn <kibadikennedy@gmail.com>
2021-06-06 04:12:14 +0800

    Update About.js

--- src/Components/Home/About.js
+++ src/Components/Home/About.js
@@ -11,7 +11,7 @@ function About() {
                 <p className="text-justify text-white text-md md:text-lg font-sans font-normal p-3">
                     <span role="img"> 💡 </span> I am a Software Developer based in Beijing. I build the App, and make it work for a better User Experience, using Responsive and UI Design approaches; I integrate third-party libraries and structure Content Model architectures for Softwares using React JS frameworks, Headless CMS and GraphQL and I always learn and improve myself with new tools. <br/><br/>
 
-                    <span role="img"> 🚀 </span> I'm available for Freelance/Jobs/Contract based projects with People. <br/><br/>
+                    <span role="img"> 🚀 </span> You can reach me out, for Freelance/Jobs/Contract based projects with People. <br/><br/>
 
 
                     <span role="img"> 📚 </span> Reading Books and Learning new things on Internet is what can describe me the most: I love Learning and working on Missions. <br/> <br/>
