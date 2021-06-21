d90fe9c6d85d1a386afe9be9e0e844e7bc8b7421
Kenn <kibadikennedy@gmail.com>
2021-06-21 23:45:52 +0800

    Update Works.js

--- src/Components/Projects/Works.js
+++ src/Components/Projects/Works.js
@@ -8,7 +8,7 @@ const Works = () => {
 
     const data = useStaticQuery(graphql`
         query MyQuery {
-            allContentfulClientProject(sort: {fields: [date], order: DESC}) {
+            allContentfulClientProject(sort: {fields: [publishDate], order: DESC}) {
                 edges {
                     node {
                         name
